30ce93bd8c48f5ccb37dd7ed3baa6d020bb821aa
Son Hong Do <sondh@gviet.vn>
2020-12-28 22:35:58 +0700

    commend spec

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,13 +1,13 @@
-import HelloWorld from "main/components/HelloWorld.vue";
-import { shallowMount } from "@vue/test-utils";
+// import HelloWorld from "main/components/HelloWorld.vue";
+// import { shallowMount } from "@vue/test-utils";
 
-describe("aaa", () => {
-  test("should ", () => {
-    const wrapper = shallowMount(HelloWorld, {
-      props: {
-        msg: "hello,vue3",
-      },
-    });
-    expect(wrapper.text()).toMatch("hello,vue3");
-  });
-});
+// describe("aaa", () => {
+//   test("should ", () => {
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: {
+//         msg: "hello,vue3",
+//       },
+//     });
+//     expect(wrapper.text()).toMatch("hello,vue3");
+//   });
+// });
